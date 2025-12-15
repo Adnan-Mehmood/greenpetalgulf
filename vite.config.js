5e6b9febb29eff8ce2a9f646ce331a3bee64b7e3
Adnan-Mehmood <ad_nan12@hotmail.com>
2025-12-15 19:33:35 +0500

    update gitignore and vite.config
    Update gitignore and vite.config file to create a build folder

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // ensure npm run build outputs to build/ instead of dist/
+    emptyOutDir: true,
+  },
 });
 
